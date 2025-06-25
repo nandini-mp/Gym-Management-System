54b8a3e82be296c6c9c7ee9a2e39faf8dc2183fd
nandini-mp <nandinimprakash.06@gmail.com>
2025-06-26 01:45:40 +0530

    Update gymStreamlit.py

--- gymStreamlit.py
+++ gymStreamlit.py
@@ -7,7 +7,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Welcome@123",
+        password=<password>,
         database="gym"
     )
 
